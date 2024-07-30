8fb33a02a0a71fb5374625ae32c1914d68d6af69
kumardhanesh950 <65038899+kumardhanesh950@users.noreply.github.com>
2024-07-30 18:06:47 +0000

    main

---
+++ app.js
@@ -0,0 +1,223 @@
+// CHAPTER 01 (ALERTS)
+// (1)
+// alert("My firstName is Dhanesh");
+// alert("My lastName is kumar");
+// alert("My email is kumardhanesh950@gmail.com");
+// alert("My PhoneNumber is 03048713318");
+// alert("My Password is xyz");
+
+// (2)
+// correct statment is
+// alert("You're learning Javascript!");
+// alert("Today i'm so excited and curious about this web development journey");
+
+// CHAPTER 02 (VARIABLES FOR STRING)
+//(1) 
+var myName ="Dhanesh"
+
+//(2)
+var favSport;
+favSport="cricket"
+// alert(favSport)
+
+//(3)
+var teamName="Pakistan team"
+// alert(teamName)
+
+//(4)
+var bestMan="Charlie";
+bestMan="Charlie Chappline";
+// alert(bestMan)
+
+
+//Chapter 03 (variables for numbers)
+//(1)
+var caseQty;
+//(2)
+caseQty = 144;
+//(3)
+var num="9";
+//(4)
+var num_1;
+num_1= 3 + 2;
+
+//(5)
+//what is the value of orderTotal?
+var merchTotal=100;
+var shippingCharge=10;
+var orderTotal= merchTotal + shippingCharge;
+// alert(orderTotal)
+
+//(6)
+var numA = 6;
+var numB= numA + 10;
+
+//Chapter 04 (Variable names legal and illegal)
+
+//1) Correct this statement
+var productCost =3.45;
+ 
+// 2)
+var name_Of_Band;
+
+//3)
+var $myAge= 26;
+
+// 4)
+var myName= "dhaneshKumar"
+
+// 5)
+//LEGAL VARIABLE
+
+// var Hello;
+// var Hello_to_programming; (space and Hyphen (-) not allowed)
+// var $hello;
+// var _hello;
+// var hello123;( Can not start)
+
+//ILLEGAL VARIABLE
+
+// var hello-world (not valid)
+// var 123hello (not valid)
+// var hello world (not allowed)
+
+//NOTE; Reserved keywords not Allowed
+
+
+//CHAP 05(MATH EXPRESSION 1)
+// (1) what is the name and symbol of arthimatic operator that gives you the remainder when one number is divided by another?
+
+// 1) remainder (%)
+
+// 2) var num = 20 % 6; //Ans is 2
+
+// 3) 
+var largeNum =1000*2000
+// alert(largeNum);
+
+// (4)
+var the_value = a - b
+var a;
+var b;
+// alert(the_value)
+
+// (5)
+var the_remainder_val= 10%3
+// alert(the_remainder_val)
+
+// (6)
+
+ var num1=2 //prompt("please enter num01")
+var num2 = 3//prompt("enter num2")
+// alert(num1 * num2)
+
+//CHAPTER (Math Expression II)
+
+// (1)
+ var x =x+1  
+ var x= x++ //(post increament) 
+ 
+
+ //(2)
+ var x= 100
+ x =--x
+//  document.write(x) =99 
+
+ //3)
+ var x=50;
+ var y=x++;
+//  document.write(y) =50 (because it's post increament)
+
+//4)
+var y =50;
+var z= --y;
+// document.write(z) =49 (because it's pre-decreament)
+
+//5)
+
+
+//CHAPTER 07 (Math Expression III)
+// 01)
+var calculatedNum = 2+(2*6)  //14 ANS 
+// document.write(calculatedNum) 
+//02)
+var calculatedNum = (2+2)*6 //24 ANS
+// document.write(calculatedNum)
+//03)
+var calculatedNum =(2+2)*(4+2) // 24 ANS
+// document.write(calculatedNum)
+//04)
+var calculatedNum =((2+2)*4)+2 // 18 ANS
+// document.write(calculatedNum)
+//05
+var statment_producing_56 =(2 + 2)*(4 + 10);
+// 06)
+var statment_producing_20 = 2 + (2 * 4) +10;
+// 07)
+
+
+
+
+//CHAPTER 08 (Concatenating Text Strings)
+// 01)
+var num = "2"+ "3"
+// document.write('"'+ num + '"')
+
+// 02)
+message = ("hello," + "dolly")
+// alert(message) // hello,dolly
+
+//03)
+// alert("33" +3); //333
+
+//04)
+// alert("Pakistan" +" "+"zindabad" )
+
+// 05) 
+var name = "dhanesh"
+var num = 950
+var nameIs= name + num
+// document.write(nameIs)
+
+
+
+
+//CHAPTER 09 (Prompts)
+//01)
+//  var firstName = prompt("Enter first name")
+//  document.write(firstName)
+
+// 02)
+// var country = prompt("country?" ,"china")
+
+// 03)
+// var yourName = prompt("Enter your Name") ;//the correct
+//04)
+
+
+// 05)
+
+// var your_fav_country =" America"
+// var fav_city_of_America ="NewYork"
+// var third_variable = prompt("your favourite city in" + your_fav_country +"?", fav_city_of_America )
+
+// 06)
+// var Message= prompt("enter Your msg" , "msg")
+// alert(Message)
+
+
+//CHAP 10 (if statments)
+// 01)
+var city ="karachi";
+if (city= "karachi"){
+    console.log("The City of Lights")
+}
+
+// 02)
+// var x=y;
+// if (x===y){
+//   var another=  prompt("what is the value of z?",)
+// }
+// document.write("The value of z is "+ another)
+
+//03)
